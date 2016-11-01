d35c7222350a86308b7b62b55c177f0cce902a0f
靳东升 <jin123d@163.com>
2016-11-01 18:29:14 +0800

    first commit

---
+++ app/src/main/java/com/jin123d/takephoto/MainActivity.java
@@ -0,0 +1,172 @@
+package com.jin123d.takephoto;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 10002;
+    private static final int PICK_ACTIVITY_REQUEST_CODE = 10003;
+    private static final int CROP_ACTIVITY_REQUEST_CODE = 10008;
+
+    private ImageView imageView;
+
+    private String imageFilePath; //拍照和选择照片后图片路径
+    private File cropFile; //裁剪后的图片文件
+    private Uri pickPhotoImageUri; //API22以下相册选择图片uri
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn_pick = (Button) findViewById(R.id.btn_pick);
+        Button btn_take = (Button) findViewById(R.id.btn_take);
+        imageView = (ImageView) findViewById(R.id.image_view);
+        btn_take.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePhoto();
+            }
+        });
+
+        btn_pick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pickPhoto();
+            }
+        });
+    }
+
+
+    //拍照获取图片
+    private void takePhoto() {
+        String state = Environment.getExternalStorageState();
+        if (state.equals(Environment.MEDIA_MOUNTED)) {
+            File imageFile = new File(Environment.getExternalStorageDirectory(), "/temp/" + System.currentTimeMillis() + ".jpg");
+            if (!imageFile.getParentFile().exists()) imageFile.getParentFile().mkdirs();
+            imageFilePath = imageFile.getPath();
+
+            //兼容性判断
+            Uri imageUri;
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+                imageUri = Util.file2Uri(this, imageFile);
+            } else {
+                imageUri = Uri.fromFile(imageFile);
+            }
+            Intent intent = new Intent();
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);//将拍取的照片保存到指定URI
+
+            List<ResolveInfo> resInfoList = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+            for (ResolveInfo resolveInfo : resInfoList) {
+                String packageName = resolveInfo.activityInfo.packageName;
+                grantUriPermission(packageName, imageUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            }
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    //从相册中取图片
+    private void pickPhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICK_ACTIVITY_REQUEST_CODE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //CropHelper.handleResult(this, requestCode, resultCode, data);
+        switch (requestCode) {
+            case CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE:
+                //拍照
+                if (resultCode == Activity.RESULT_OK) {
+                    crop(false);
+                }
+                break;
+
+            case CROP_ACTIVITY_REQUEST_CODE:
+                //裁剪完成
+                if (data != null) {
+                    Bitmap bitmap;
+                    try {
+                        bitmap = BitmapFactory.decodeFile(cropFile.getPath());
+                        imageView.setImageBitmap(bitmap);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+
+            case PICK_ACTIVITY_REQUEST_CODE:
+                //从相册选择
+                if (data != null && resultCode == Activity.RESULT_OK) {
+                    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+                        imageFilePath = Util.getPathByUri4kitkat(this, data.getData());
+                    } else {
+                        pickPhotoImageUri = data.getData();
+                    }
+
+                    crop(true);
+                }
+                break;
+        }
+
+    }
+
+
+    /**
+     * 裁剪
+     *
+     * @param isPick 是否是从相册选择
+     */
+    private void crop(boolean isPick) {
+        cropFile = new File(Environment.getExternalStorageDirectory(), "/temp/" + System.currentTimeMillis() + ".jpg");
+        if (!cropFile.getParentFile().exists()) cropFile.getParentFile().mkdirs();
+        Uri outputUri, imageUri;
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
+            outputUri = Util.file2Uri(this, cropFile);
+            imageUri = Util.file2Uri(this, new File(imageFilePath));
+        } else {
+            outputUri = Uri.fromFile(cropFile);
+            imageUri = isPick ? pickPhotoImageUri : Uri.fromFile(new File(imageFilePath));
+        }
+
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(imageUri, "image/*");
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        intent.putExtra("scale", true);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputUri);
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        intent.putExtra("noFaceDetection", true); // no face detection
+
+        //授予"相机"保存文件的权限 针对API24+
+        List<ResolveInfo> resInfoList = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        for (ResolveInfo resolveInfo : resInfoList) {
+            String packageName = resolveInfo.activityInfo.packageName;
+            grantUriPermission(packageName, outputUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        }
+        startActivityForResult(intent, CROP_ACTIVITY_REQUEST_CODE);
+    }
+
+
+}
